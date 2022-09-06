b757830458cfbab494097d8b18199cfa537de089
Cefnar Auyon <juliorecinos30@gmail.com>
2022-09-06 16:53:20 -0600

    All done until 1 hour 15 minutes

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.jpg'
+import HeaderSocial from './HeaderSocials'
+
+const Header = () => {
+    return (
+       
+        <header>
+            <div id='home' className="container header__container">
+                <h5>Hello I'm</h5>
+                <h1>Julio Cefnar Auyon</h1>
+                <h5 className="text-light">Fullstack Developer</h5>
+               <CTA/>
+               <HeaderSocial />
+
+               <div className="me">
+                <img src={ME} alt="Julio Cefnar Auyon" />
+               </div>
+                <a href="#contact" className="scroll__down">Scroll down</a>
+            </div>
+
+
+        </header>
+
+
+
+
+
+    )
+
+
+
+}
+export default Header
